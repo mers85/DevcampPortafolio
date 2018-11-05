dfb212041931dc8c30e38a36cbd3db934e221f3b
Mayra Rodriguez <mers85@gmail.com>
2018-11-05 16:32:32 -0400

    Implemented basic authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :portfolios, except: [:show]
   get 'vuejs-items', to: 'portfolios#vuejs'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
